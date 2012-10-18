75e3fc7e3dabd47b39ce788e1b645abf9b3aba8b
Regie G. Pinat <regiepinat@KlabCyscorpionss-Mac-mini-3.local>
2012-10-18 16:07:32 +0800

    Initial Commit

---
+++ CrazyRocketManApp/CrazyRocketManAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CrazyRocketManAppAppDelegate.h
+//  CrazyRocketManApp
+//
+//  Created by Regie G. Pinat on 10/18/12.
+//  Copyright (c) 2012 Regie G. Pinat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CrazyRocketManAppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrazyRocketManApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrazyRocketManApp
+//
+//  Created by Regie G. Pinat on 10/18/12.
+//  Copyright (c) 2012 Regie G. Pinat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CrazyRocketManAppAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CrazyRocketManAppAppDelegate class]));
+    }
+}
